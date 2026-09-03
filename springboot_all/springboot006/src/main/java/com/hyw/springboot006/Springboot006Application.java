e180b2464f9f1ada36778eff28e7b8748231b07b
jekny <2656803448@qq.com>
2026-09-03 11:08:54 +0800

    创建一个项目练习配置文件内容

---
+++ springboot_all/springboot006/src/main/java/com/hyw/springboot006/Springboot006Application.java
@@ -0,0 +1,13 @@
+package com.hyw.springboot006;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot006Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot006Application.class, args);
+    }
+
+}
