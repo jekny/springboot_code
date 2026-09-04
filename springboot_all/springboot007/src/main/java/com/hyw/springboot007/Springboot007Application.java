bc6880a2f7f59758156d3f5ffff2778874392205
jekny <2656803448@qq.com>
2026-09-04 10:27:05 +0800

    方式3：@ConfigurationPropertiesScan({"com.hyw.springboot007.entity"})

--- springboot_all/springboot007/src/main/java/com/hyw/springboot007/Springboot007Application.java
+++ springboot_all/springboot007/src/main/java/com/hyw/springboot007/Springboot007Application.java
@@ -5,10 +5,12 @@ import jakarta.annotation.Resource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({Costomer.class})
+//@EnableConfigurationProperties({Costomer.class})
+@ConfigurationPropertiesScan({"com.hyw.springboot007.entity"})
 public class Springboot007Application {
     public static void main(String[] args) {
         SpringApplication.run(Springboot007Application.class, args);
