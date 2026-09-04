c74756d993cdfc2d89208e1009a6a5ab465caa03
jekny <2656803448@qq.com>
2026-09-04 11:51:23 +0800

    创建008demo练习整合mybatis

---
+++ springboot_all/springboot008/src/main/java/com/hyw/springboot008/Springboot008Application.java
@@ -0,0 +1,13 @@
+package com.hyw.springboot008;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot008Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot008Application.class, args);
+    }
+
+}
---
+++ springboot_all/springboot008/src/test/java/com/hyw/springboot008/Springboot008ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyw.springboot008;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot008ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
