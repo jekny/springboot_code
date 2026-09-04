ae5b9937b1e38da6ac7081aeb86586ce7875ad59
jekny <2656803448@qq.com>
2026-09-04 10:00:43 +0800

    创建007demo

---
+++ springboot_all/springboot007/src/main/java/com/hyw/springboot007/Springboot007Application.java
@@ -0,0 +1,13 @@
+package com.hyw.springboot007;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot007Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot007Application.class, args);
+    }
+
+}
---
+++ springboot_all/springboot007/src/test/java/com/hyw/springboot007/Springboot007ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyw.springboot007;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot007ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
