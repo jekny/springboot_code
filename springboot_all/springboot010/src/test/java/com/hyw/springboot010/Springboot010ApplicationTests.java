808a16749e4ac403a8249d37134efb774a0e62fd
jekny <2656803448@qq.com>
2026-09-16 12:12:06 +0800

    创建010demo，练习springboot整合mybatis

---
+++ springboot_all/springboot010/src/test/java/com/hyw/springboot010/Springboot010ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyw.springboot010;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot010ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
