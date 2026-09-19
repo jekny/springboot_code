e2d97dab9dab6f4fa6a03472109f850e7022f754
jekny <2656803448@qq.com>
2026-09-19 14:40:26 +0800

    创建项目

---
+++ springboot_all/KaoShi/src/main/java/com/example/kaoshi/KaoShiApplication.java
@@ -0,0 +1,13 @@
+package com.example.kaoshi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaoShiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KaoShiApplication.class, args);
+    }
+
+}
