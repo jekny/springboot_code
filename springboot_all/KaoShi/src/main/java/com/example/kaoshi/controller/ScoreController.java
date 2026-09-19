cc0d0aacf1d3e7cd4535d2ae1a2f936f1a148871
jekny <2656803448@qq.com>
2026-09-19 16:43:28 +0800

    实现增删查接口

--- springboot_all/KaoShi/src/main/java/com/example/kaoshi/controller/ScoreController.java
+++ springboot_all/KaoShi/src/main/java/com/example/kaoshi/controller/ScoreController.java
@@ -7,8 +7,7 @@ import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,5 +30,25 @@ public class ScoreController {
         List<StudentScore> list = studentScoreService.list();
         model.addAttribute("scoreList",list);
         return "index";
+    }
+
+    @PostMapping("/add")
+    public String addStu(StudentScore studentScore){//为什么这里参数不用写注解，前端是提交到后端的一条一条信息，后端是如何拿到数据的呢
+        studentScoreService.save(studentScore);
+        return "redirect:/score/list";
+    }
+
+    @GetMapping("/detail")
+    public String detailstu(@RequestParam Long id,Model model){
+        StudentScore studentScore = studentScoreService.getById(id);
+        model.addAttribute("score",studentScore);
+        return "score_detail";
+    }
+
+    @GetMapping("/delete")
+    public String deletestu(@RequestParam Long id){
+        studentScoreService.removeById(id);
+        return "redirect:/score/list";
+
     }
 }
